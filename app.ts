5f768a513053c13fb3775650574c464d1d286db5
Stas A <altgov3en@gmail.com>
2020-04-18 13:08:54 +0300

    intial project upload

---
+++ app.ts
@@ -0,0 +1,7 @@
+import { Category } from './enums';
+import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
+import { UniversityLibrarian, ReferenceItem } from './classes';
+import * as util from './lib/utilityFunctions';
+
+
+console.log('Hello from advanced TS course!!!!');
