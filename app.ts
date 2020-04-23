fcfbbbca994ee9e0b260a4ea11c2274264f74dea
212768867 <stanislav.altgovzen@ge.com>
2020-04-23 17:47:39 +0300

    section added accordingly to the course

--- app.ts
+++ app.ts
@@ -24,29 +24,51 @@ const schoolBook: Book[] = [
     { id: 12, author: 'Fyodor Dostoyevsky', title: 'Crime and Punishment', available: true, category: Category.History }
 ]
 
-const allBooks = [...util.GetAllBooks(), ...schoolBook];
+/**
+ * Destructuring arrays and objects
+ * Spread Operators
+ */
+// const allBooks = [...util.GetAllBooks(), ...schoolBook];
 
-let poets = ['Shelley', 'Collins', 'Hughes'];
-let authors = ['Tolstoy', 'Fitzgerald', ...poets];
-
-
-const book = allBooks[1];
-let catalogLocation: [string, Book] = ['A 325.213', book];
-
-console.log(catalogLocation[0]);
-console.log(catalogLocation[1]);
+// let poets = ['Shelley', 'Collins', 'Hughes'];
+// let authors = ['Tolstoy', 'Fitzgerald', ...poets];
 
 
-interface KeyValuePair<K, V> extends Array<K | V> {
-    0: K;
-    1: V;
-}
 
-const book1 = allBooks[2];
-let catalogLocation1: KeyValuePair<string, Book> = ['A 325.213', book1];
+/**
+ * Tuple Types
+ */
+// const allBooks = [...util.GetAllBooks(), ...schoolBook];
+// const book = allBooks[1];
+// let catalogLocation: [string, Book] = ['A 325.213', book];
 
-console.log(catalogLocation1[0]);
-console.log(catalogLocation1[1]);
+// console.log(catalogLocation[0]);
+// console.log(catalogLocation[1]);
+
+// interface KeyValuePair<K, V> extends Array<K | V> {
+//     0: K;
+//     1: V;
+// }
+
+// const book1 = allBooks[2];
+// let catalogLocation1: KeyValuePair<string, Book> = ['A 325.213', book1];
+
+// console.log(catalogLocation1[0]);
+// console.log(catalogLocation1[1]);
+
+/**
+ * Union and Intersection Types
+ */
+// const allBooks: Book[] = util.GetAllBooks();
+// const allCatalogs: Magazine[] = util.GetAllMagazines();
 
 
+// const readingItem: Magazine | Book = allBooks[0];
+
+// function printReadingItem(item: Magazine | Book){
+//     console.log(JSON.stringify(item));
+// }
+
+// printReadingItem(allCatalogs[0]);
+// printReadingItem(allBooks[0]);
 
