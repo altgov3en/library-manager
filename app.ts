ea159ab3592a2fd6d3abba4e36f6a94ccfde42c6
Stas A <altgov3en@gmail.com>
2020-04-25 13:39:45 +0300

    aligning material

--- app.ts
+++ app.ts
@@ -3,21 +3,6 @@ import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
 import { UniversityLibrarian, ReferenceItem } from './classes';
 import * as util from './lib/utilityFunctions';
 
-function printBookInfo({ title, author }: Book) {
-    console.log(`'${title}' was authored by '${author}'`);
-}
-
-// let [book1, book2, ...rest]: Book[] = util.GetAllBooks();
-
-// printBookInfo(book1);
-// printBookInfo(book2);
-// rest.forEach(i => printBookInfo(i));
-
-// let { title: specialTitle, author: specialAuthor } = book1;
-
-// console.log(specialTitle);
-// console.log(specialAuthor);
-
 const schoolBook: Book[] = [
     { id: 10, author: 'J.K. Rowling', title: 'Harry Potter and The Goblet of Fire', available: true, category: Category.Children },
     { id: 11, author: 'Anita Desai', title: 'Clear Light of Day', available: true, category: Category.Fiction },
@@ -28,11 +13,44 @@ const schoolBook: Book[] = [
  * Destructuring arrays and objects
  * Spread Operators
  */
+//  //destructuring array into two separate objects
+// const [book1, book2] = util.GetAllBooks();
+// printBookInfo(book1);
+// printBookInfo(book2);
+// console.log('');
+// printBookInfo2(book1);
+// printBookInfo2(book2);
+// console.log('');
+// printBookInfo3(book1);
+// printBookInfo3(book2);
+
+// logFavoriteBooks(util.GetAllBooks());
+
+// // using spread operators
 // const allBooks = [...util.GetAllBooks(), ...schoolBook];
 
 // let poets = ['Shelley', 'Collins', 'Hughes'];
 // let authors = ['Tolstoy', 'Fitzgerald', ...poets];
 
+// function printBookInfo(item: Book) {
+//     console.log(`${item.title} was authored by ${item.author}`);
+// }
+
+// // destructuring object with default names
+// function printBookInfo2({ title, author }: Book) {
+//     console.log(`${title} was authored by ${author}`);
+// }
+
+// // destructuring object with custom names
+// function printBookInfo3({ title: bookTitle, author: bookAuthor }: Book) {
+//     console.log(`${bookTitle} was authored by ${bookAuthor}`);
+// }
+
+// // destructuring array into two separate objects and 'others' array
+// function logFavoriteBooks([book1, book2, ...other]: Book[]): void {
+//     console.log(book1);
+//     console.log(book2);
+// }
 
 
 /**
@@ -71,4 +89,3 @@ const schoolBook: Book[] = [
 
 // printReadingItem(allCatalogs[0]);
 // printReadingItem(allBooks[0]);
-
